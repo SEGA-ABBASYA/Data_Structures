34b151491381ec3ed9157bfc55af5e6c214f60d2
Ziad Khaled <ziadshraf100@gmail.com>
2024-04-20 03:35:30 +0200

    Test VS

---
+++ WranaSection/out/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_WranaSection_FILE E:/projects/Data_Structures/WranaSection/out/build/WranaSection.exe)
+set(__QT_DEPLOY_TARGET_WranaSection_RUNTIME_DLLS C:/Qt/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
