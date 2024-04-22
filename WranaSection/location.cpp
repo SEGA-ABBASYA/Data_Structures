649b69a29eb22e91369ff1550bc2c866445311db
Ziad Khaled <ziadshraf100@gmail.com>
2024-04-23 00:21:58 +0200

    The Location class

---
+++ WranaSection/location.cpp
@@ -0,0 +1,101 @@
+#include "location.h"
+
+Location::Location(string name, queue<string> doctor, bool midterm, string type, int section, bool group, map<string, pair<int, int>> date, string department)
+{
+    this->name = name;
+    this->doctor = doctor;
+    this->midterm = midterm;
+    this->type = type;
+    this->section = section;
+    this->group = group;
+    this->date = date;
+    this->department = department;
+}
+
+//Setters
+void Location::setName(string name)
+{
+    this->name = name;
+}
+
+void Location::setDoctor(queue<string> doctor)
+{
+    this->doctor = doctor;
+}
+
+void Location::setSection(int section)
+{
+    this->section = section;
+}
+
+void Location::setGroup(bool group)
+{
+    this->group = group;
+}
+
+void Location::setMidterm(bool midterm)
+{
+    this->midterm = midterm;
+}
+
+void Location::setType(string type)
+{
+    this->type = type;
+}
+
+void Location::setDepartment(string department)
+{
+    this->department = department;
+}
+
+void Location::setDate(map<string, pair<int, int>> date)
+{
+    this->date = date;
+}
+
+
+//Getters
+string Location::getName()
+{
+    return name;
+}
+
+queue<string> Location::getDoctor()
+{
+    return doctor;
+}
+
+int Location::getSection()
+{
+    return section;
+}
+
+int Location::getGroup()
+{
+    return group;
+}
+
+map<string, pair<int, int>> Location::getDate()
+{
+    return date;
+}
+
+bool Location::getMidterm()
+{
+    return midterm;
+}
+
+string Location::getType()
+{
+    return type;
+}
+
+string Location::getDepartment()
+{
+    return department;
+}
+
+
+Location::~Location()
+{
+}
---
+++ WranaSection/location.h
@@ -0,0 +1,62 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+#include <string>
+#include <queue>
+#include <map>
+using namespace std;
+
+class Location
+{
+    //refers to location
+    string name;
+
+    //teaching this lab or lecture
+    queue<string> doctor;
+
+    //if there are two doctors one before mid and another after
+    //0 before, 1 after
+    bool midterm;
+
+    //Lab, section or lecture
+    string type;
+
+    int section;
+
+    //0 for G1 , 1 for G2
+    bool group;
+
+    //Mainstream for years 1,2 and from years 3,4 (CS, IS, SC, CSYS)
+    string department;
+
+    // Day, Hours and minutes
+    map<string, pair<int, int>> date;
+
+    //Course course;
+
+public:
+    Location(string, queue<string>, bool, string, int, bool, map<string, pair<int, int>>, string);
+
+    void setName(string);
+    void setDoctor(queue<string>);
+    void setSection(int);
+    void setGroup(bool);
+    void setDate(map<string, pair<int, int>>);
+    void setMidterm(bool);
+    void setType(string);
+    void setDepartment(string);
+
+
+    string getName();
+    queue<string> getDoctor();
+    int getSection();
+    int getGroup();
+    map<string, pair<int, int>> getDate();
+    bool getMidterm();
+    string getType();
+    string getDepartment();
+
+    ~Location();
+
+};
+
+#endif // LOCATION_H
