c6b5c939e646a76709409fdd31541c95b09b9290
Rahma <rahmakhattab07@gmail.com>
2024-04-22 21:55:32 +0200

    Created base class Admin as demonstrated in primary UML.

---
+++ WranaSection/Admin.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include"Admin.h"
+using namespace std;
+
+// Constructor
+Admin::Admin()
+{
+    username = "Admin";
+    password = "Admin";
+}
+
+// Getter, and setter for username.
+string Admin::getUsername()
+{
+    return username;
+}
+void Admin::setUsername(string newUsername)
+{
+    username = newUsername;
+}
+
+// Getter, and setter for password.
+string Admin::getPassword()
+{
+    return password;
+}
+void Admin::setPassword(string newPassword)
+{
+    password = newPassword;
+}
+
+// Course class related methods.
+void Admin::editCourse(Course course)
+{
+    // Body
+}
+void Admin::addCourse(Course course)
+{
+    // Body
+}
+void Admin::deleteCourse(Course course)
+{
+    // Body
+}
+
+// Loction class related methods.
+void Admin::addLocation(Location location)
+{
+    // Body
+}
+void Admin::editLocation(Location location)
+{
+    // Body
+}
+void Admin::removeLocation(Location location)
+{
+    // Body
+}
+
+void Admin::mangoElAb()
+{
+    cout << "Mango world";
+}
+
+// Destructor
+Admin::~Admin()
+{
+
+}
---
+++ WranaSection/Admin.h
@@ -0,0 +1,43 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <bits/stdc++.h>
+using namespace std;
+class Admin
+{
+
+private:
+
+    string username;
+    string password;
+
+public:
+
+    // Constructor
+    Admin();
+
+    // Getter, and setter for username.
+    string getUsername();
+    void setUsername(string newUsername);
+
+    // Getter, and setter for password.
+    string getPassword();
+    void setPassword(string newPassword);
+
+    // Course class related methods.
+    void editCourse(Course course);
+    void addCourse(Course course);
+    void deleteCourse(Course course);
+
+    // Loction class related methods.
+    void addLocation(Location location);
+    void editLocation(Location location);
+    void removeLocation(Location location);
+
+    void mangoElAb();
+
+    // Destructor
+    ~Admin();
+};
+
+#endif // ADMIN_H
